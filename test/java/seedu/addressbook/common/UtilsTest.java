3cfab21cecec2c5f88560794437f5b2b4fd92f2b
Irvin Lim <irvinlimweiquan@gmail.com>
2016-09-08 04:41:22 +0800

    Create test cases for elementsAreUnique

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class UtilsTest {
 
 	@Test
-	public void nullArray_returnsCorrect() {
+	public void isAnyNull_nullArray() {
 		Object[] test = { null, null, null };
 		boolean actual = Utils.isAnyNull(test);
 		
@@ -17,7 +17,7 @@ public class UtilsTest {
 	}
 
 	@Test
-	public void singleNullInArray_returnsCorrect() {
+	public void isAnyNull_singleNullInArray() {
 		Object[] test = { new ArrayList<String>(), null, new Object() };
 		boolean actual = Utils.isAnyNull(test);
 		
@@ -25,11 +25,59 @@ public class UtilsTest {
 	}
 
 	@Test
-	public void nonNullArray_returnsIncorrect() {
+	public void isAnyNull_nonNullArray() {
 		Object[] test = { new ArrayList<String>() };
 		boolean actual = Utils.isAnyNull(test);
 		
 		assertEquals(actual, false);
+	}
+
+	@Test
+	public void isAnyNull_emptyArray() {
+		Object[] test = {};
+		boolean actual = Utils.isAnyNull(test);
+		
+		assertEquals(actual, false);
+	}
+
+	@Test
+	public void elementsAreUnique_singleElementArray() {
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("hello");
+		
+		boolean actual = Utils.elementsAreUnique(test);
+		
+		assertEquals(actual, true);
+	}
+
+	@Test
+	public void elementsAreUnique_twoUniqueElementsInArray() {
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("hello");
+		test.add("world");
+		
+		boolean actual = Utils.elementsAreUnique(test);
+		
+		assertEquals(actual, true);
+	}
+
+	@Test
+	public void elementsAreUnique_twoEqualElementsInArray() {
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("hello");
+		test.add("hello");
+		
+		boolean actual = Utils.elementsAreUnique(test);
+		
+		assertEquals(actual, false);
+	}
+
+	@Test
+	public void elementsAreUnique_emptyArray() {
+		ArrayList<String> test = new ArrayList<String>();
+		boolean actual = Utils.elementsAreUnique(test);
+		
+		assertEquals(actual, true);
 	}
 
 }
